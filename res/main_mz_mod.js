416f1d78964b776ededce223df3d47059f5ee8c8
Synrec <kylestclr@gmail.com>
2026-07-17 22:28:07 -0400

    Apparently audio appears to be preloaded, need feedback before continuing.

--- res/main_mz_mod.js
+++ res/main_mz_mod.js
@@ -17,6 +17,49 @@ const scriptUrls = [
     "js/plugins.js"
 ];
 const effekseerWasmUrl = "js/libs/effekseer.wasm";
+const initial_master_editor_plugin_data = [];
+
+function CONVERT_EDITOR_JSON_TO_PARAMS(json) {
+    const obj = {};
+    const keys = Object.keys(json);
+    keys.forEach((key) => {
+        const data = json[key];
+        if (data.gen) {
+            if (data.list) {
+                obj[key] = JSON.stringify(
+                    data.list.map((arr_itm) => {
+                        const gen = arr_itm.gen;
+                        if (gen.no_struct) {
+                            return gen.value;
+                        } else {
+                            const converted_gen = CONVERT_EDITOR_JSON_TO_PARAMS(gen);
+                            return converted_gen;
+                        }
+                    })
+                )
+            } else {
+                const no_struct = data.gen.no_struct;
+                if (no_struct) {
+                    const value = data.gen.value;
+                    if (value) {
+                        obj[key] = value.toString();
+                    } else {
+                        obj[key] = value;
+                    }
+                } else {
+                    obj[key] = CONVERT_EDITOR_JSON_TO_PARAMS(data.gen);
+                }
+            }
+        } else {
+            if (data.value) {
+                obj[key] = data.value.toString();
+            } else {
+                obj[key] = data.value;
+            }
+        }
+    })
+    return JSON.stringify(obj);
+}
 
 class Main {
     constructor() {
@@ -49,6 +92,7 @@ class Main {
     }
 
     testXhr() {
+        console.log(this)
         const xhr = new XMLHttpRequest();
         xhr.open("GET", document.currentScript.src);
         xhr.onload = () => (this.xhrSucceeded = true);
@@ -76,6 +120,8 @@ class Main {
             document.body.appendChild(script);
         }
         this.numScripts = scriptUrls.length;
+        preload.start();
+        this._preload_mode = true;
         window.addEventListener("load", this.onWindowLoad.bind(this));
         window.addEventListener("error", this.onWindowError.bind(this));
     }
@@ -111,6 +157,10 @@ class Main {
     }
 
     onWindowLoad() {
+        if (this._preload_mode) {
+            requestAnimationFrame(this.onWindowLoad.bind(this));
+            return;
+        }
         if (!this.xhrSucceeded) {
             const message = "Your browser does not allow to read local files.";
             this.printError("Error", message);
@@ -160,65 +210,68 @@ class Preload {
         this._plugin_list = [];
         this._image_list = [];
         this._audio_list = [];
-        this.startDataPreload();
     }
 
-    startDataPreload() {
+    start() {
         this.drawLoadingScreen();
         this.generatePluginConfigList();
         this.generateImageList();
         this.generateAudioList();
+        this.updateDataPreload();
     }
 
     async drawLoadingScreen() { }
 
     async generatePluginConfigList() {
-        this._generating_plugin_list = true;
+        const preloader = this;
+        preloader._generating_plugin_list = true;
         const url = `js/plugins/_master_editor.txt`;
         const xhr = new XMLHttpRequest();
         xhr.open("GET", url);
         xhr.onload = async function () {
             const res_str = xhr.responseText;
             const list_arr = JSON.parse(res_str);
-            this._plugin_list = list_arr;
-            this._generating_plugin_list = false;
+            preloader._plugin_list = list_arr;
+            preloader._generating_plugin_list = false;
         }
         xhr.onerror = async function () {
-            this._generating_plugin_list = false;
+            preloader._generating_plugin_list = false;
         }
         await xhr.send();
     }
 
     async generateImageList() {
-        this._generating_image_list = true;
+        const preloader = this;
+        preloader._generating_image_list = true;
         const url = `img/_master_editor.txt`;
         const xhr = new XMLHttpRequest();
         xhr.open("GET", url);
         xhr.onload = async function () {
             const res_str = xhr.responseText;
             const list_arr = JSON.parse(res_str);
-            this._image_list = list_arr;
-            this._generating_image_list = false;
+            preloader._image_list = list_arr;
+            preloader._generating_image_list = false;
         }
         xhr.onerror = async function () {
-            this._generating_image_list = false;
+            preloader._generating_image_list = false;
         }
         await xhr.send();
     }
 
     async generateAudioList() {
-        this._generating_audio_list = true;
+        const preloader = this;
+        preloader._generating_audio_list = true;
         const url = `audio/_master_editor.txt`;
         const xhr = new XMLHttpRequest();
         xhr.open("GET", url);
         xhr.onload = async function () {
             const res_str = xhr.responseText;
             const list_arr = JSON.parse(res_str);
-            this._audio_list = list_arr;
-            this._generating_audio_list = false;
+            preloader._audio_list = list_arr;
+            preloader._generating_audio_list = false;
         }
         xhr.onerror = async function () {
-            this._generating_audio_list = false;
+            preloader._generating_audio_list = false;
         }
         await xhr.send();
     }
@@ -231,54 +284,102 @@ class Preload {
         )
     }
 
-    updateDataPreload() {
+    async updateDataPreload() {
         if (this.generatingLists()) {
-            requestAnimationFrame(this.updateDataPreload);
+            requestAnimationFrame(this.updateDataPreload.bind(this));
             return;
         }
-        if (this.loadingPlugins()) {
-            requestAnimationFrame(this.updateDataPreload);
+        if (await this.loadingPlugins()) {
+            requestAnimationFrame(this.updateDataPreload.bind(this));
             return;
         }
-        if (this.loadingImages()) {
-            requestAnimationFrame(this.updateDataPreload);
+        if (await this.loadingImages()) {
+            requestAnimationFrame(this.updateDataPreload.bind(this));
             return;
         }
-        if (this.loadingSounds()) {
-            requestAnimationFrame(this.updateDataPreload);
+        if (await this.loadingSounds()) {
+            requestAnimationFrame(this.updateDataPreload.bind(this));
             return;
         }
         this.endDataPreload();
     }
 
     async loadingPlugins() {
-        if (this._plugin_list.length > 0) {
+        const preloader = this;
+        if (preloader._loading_plugin_data) {
             return true;
         }
+        const src = `js/plugins/data`;
+        if (preloader._plugin_list.length > 0) {
+            const list_item = preloader._plugin_list.shift();
+            if (!list_item) return false;
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", `${src}/${list_item}.json`);
+            xhr.onload = function () {
+                const res_string = xhr.responseText;
+                const res_json = JSON.parse(res_string);
+                const param_str = CONVERT_EDITOR_JSON_TO_PARAMS(res_json);
+                const parsed_editor_parameters = JSON.parse(param_str);
+                initial_master_editor_plugin_data.push({ name: list_item.toLowerCase(), data: parsed_editor_parameters });
+                preloader._loading_plugin_data = false;
+            }
+            xhr.onerror = function () {
+                preloader._loading_plugin_data = false;
+            }
+            xhr.send();
+            preloader._loading_plugin_data = true;
+            return true;
+        }
+        return false;
     }
 
     async loadingImages() {
-        if (this._image_list.length > 0) {
+        const preloader = this;
+        if (preloader._image_list.length > 0) {
+            const list_item = preloader._image_list.shift();
+            if (!list_item) return false;
+            const img_src = document.createElement('img');
+            img_src.src = list_item;
+            document.head.appendChild(img_src);
+            document.head.removeChild(img_src);
             return true;
         }
+        return false;
     }
 
     async loadingSounds() {
-        if (this._audio_list.length > 0) {
+        const preloader = this;
+        if (this._loading_aud) {
+            const aud = this._loading_aud;
+            if (aud.readyState == 4) {
+                document.head.removeChild(aud);
+                this._loading_aud = null;
+            } else {
+                return;
+            }
+        }
+        if (preloader._audio_list.length > 0) {
+            const list_item = preloader._audio_list.shift();
+            if (!list_item) return false;
+            const aud_src = document.createElement('audio');
+            aud_src.src = list_item;
+            aud_src.preload = true;
+            document.head.appendChild(aud_src);
+            this._loading_aud = aud_src;
             return true;
         }
+        return false;
     }
 
     endDataPreload() {
         this.erasePreloadScreen();
-        main.run();
+        main._preload_mode = false;
     }
 
     erasePreloadScreen() { }
 }
-
 const main = new Main();
 const preload = new Preload();
-// main.run();
+main.run();
 
 //-----------------------------------------------------------------------------
